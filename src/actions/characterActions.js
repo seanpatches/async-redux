e024ee7010d668a6918e25ba83ab19c38e7b5f33
Sean Nelson <smnelsonn@gmail.com>
2019-05-30 17:42:18 -0700

    clean up

--- src/actions/characterActions.js
+++ src/actions/characterActions.js
@@ -1,8 +1,5 @@
 import { fetchAvatar } from '../services/fetchAvatar';
 import { createAction } from 'promise-middleware-redux';
-// export const GET_CHARACTERLIST = 'GET_CHARACTERLIST';
-// export const GET_CHARACTER_ERROR = 'GET_CHARACTER_ERROR';
-// export const CHARACTER_LOADING = 'CHARACTER_LOADING';
 
 export const [
   getCharacterList, //name of action creator
